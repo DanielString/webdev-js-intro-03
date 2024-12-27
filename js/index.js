3451d76223f89369023e5aecda8ff61d6beaa93e
Daniel Pelstring <danielpelstring@macbookpro.lan>
2024-12-27 18:00:23 -0500

    tested console log and answered the provided questions

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@
 // that are not declared
 "use strict"
 
+//console.log(7 + 6);  //Output: 13 (Adds)
+// console.log(20 - 7);  //Output: 13 (Subtracts)
+// console.log(1 + "3"); //Output: 13 (Outputs the number 1 and the string "3")
+
+// console.log(20 - 5 * 4);  //Output: 0 (PEMDAS)
+// console.log((10 + 3) * 1); //Output: 13 (PEMDAS)
+
+// console.log(12 / 6); //Output: 2 (Division)
+// console.log(23 % 5); //Output: 3 (5 goes into 20 4 times, with a remainder of 3)
+
+// console.log(13 == 13); //Output: true (13 does equal 13)
+// console.log(12 == "12"); //Output: true (12 does equal 12)
+// console.log(13 === "13"); //Output: false (The number 13 is not strictly equal to the string 13)
+
+// console.log(6 || 5); //Output: 6
+// console.log(5 || 6); //Output: 5
+// console.log(1 && 2); //Output: 2
+// console.log(1 && 1); //Output: 1
